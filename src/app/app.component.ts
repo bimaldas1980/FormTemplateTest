f9bfa78f6e94ed8c7e9a965e975b0467922fa3eb
bimaldas1980 <bimaldas.bl@gmail.com>
2018-04-01 01:12:19 +1000

    work in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild("f") formElement: NgForm;
+  subscriptionModel = {
+    email:'',
+    subscription:'',
+    password:''
+  };
+
+initializeValue() {
+  this.formElement.form.patchValue(
+    {subscription: "advanced"}
+  );
+}
+
+  OnSubmitForm() {
+    console.log(this.subscriptionModel);
+  }
+
+  onNgInit() {
+    this.initializeValue();
+  }
 }
